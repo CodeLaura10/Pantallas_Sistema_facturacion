602457d4e5533daefad97b09e265b724b2d9f974
Juan David Garcia Vargas <gvargasjd@gmail.com>
2025-08-24 22:21:42 -0500

    Ayuda y acerca de

--- frmPrincipal.Designer.cs
+++ frmPrincipal.Designer.cs
@@ -33,6 +33,7 @@
             folderBrowserDialog1 = new FolderBrowserDialog();
             label1 = new Label();
             pnlPrincipal = new Panel();
+            panelContenedor = new Panel();
             panel1 = new Panel();
             TbpMenu = new MaterialSkin.Controls.MaterialTabControl();
             principal = new TabPage();
@@ -93,11 +94,19 @@
             // pnlPrincipal
             // 
             pnlPrincipal.BackColor = SystemColors.ButtonFace;
+            pnlPrincipal.Controls.Add(panelContenedor);
             pnlPrincipal.Controls.Add(panel1);
             pnlPrincipal.Location = new Point(1, 60);
             pnlPrincipal.Name = "pnlPrincipal";
             pnlPrincipal.Size = new Size(1287, 730);
             pnlPrincipal.TabIndex = 3;
+            // 
+            // panelContenedor
+            // 
+            panelContenedor.Location = new Point(180, 3);
+            panelContenedor.Name = "panelContenedor";
+            panelContenedor.Size = new Size(1107, 723);
+            panelContenedor.TabIndex = 1;
             // 
             // panel1
             // 
@@ -390,7 +399,7 @@
             btnAyuda.TabIndex = 7;
             btnAyuda.Text = "Ayuda";
             btnAyuda.UseVisualStyleBackColor = true;
- 
+            btnAyuda.Click += btnAyuda_Click;
             // 
             // btnAcerca
             // 
@@ -407,6 +416,7 @@
             btnAcerca.TabIndex = 6;
             btnAcerca.Text = "Acerca de..";
             btnAcerca.UseVisualStyleBackColor = true;
+            btnAcerca.Click += btnAcerca_Click;
             // 
             // pictureBox4
             // 
@@ -436,9 +446,7 @@
             AutoScaleMode = AutoScaleMode.Font;
             AutoSizeMode = AutoSizeMode.GrowAndShrink;
             BackColor = SystemColors.ButtonFace;
-
-            ClientSize = new Size(1884, 1380);
-
+            ClientSize = new Size(1974, 1372);
             Controls.Add(pnlPrincipal);
             Controls.Add(TabOpcionesMenu);
             Controls.Add(label1);
@@ -501,5 +509,6 @@
         private Button btnEmpleados;
         private Button btnAyuda;
         private Button btnAcerca;
+        private Panel panelContenedor;
     }
 }
